a51182f983cde61c227cbf2e460d86392d03f7df
chatwme <159962185+chatwme@users.noreply.github.com>
2024-03-01 08:23:50 +0000

    login class

--- chatwme/src/database.js
+++ chatwme/src/database.js
@@ -17,3 +17,45 @@ firebase.initializeApp(firebaseConfig);
 // Initialize Cloud Firestore and get a reference to the service
 const db = firebase.firestore();
 
+
+class Login {
+    constructor (login_name, login_password, login_id ) {
+        this.login_name = login_name;
+        this.login_password = login_password;
+        this.login_id = login_id;
+    }
+
+    getlogin_name() {
+        return this.login_name;
+    }
+
+    getlogin_password(){
+        return this.login_password;
+    }
+    getlogin_id (){
+        return this.login.id;
+    }
+
+    setlogin_name(newname){
+        
+    }
+}
+// Firestore data converter
+var loginconverter = {
+    toFirestore: function(login) {
+        return {
+            login_name: login.login_name,
+            login_password: login.login_password,
+            login_id: login.login_id
+            };
+    },
+    fromFirestore: function(snapshot, options){
+        const data = snapshot.data(options);
+        return new Login(data.login_name, data.login_password, data.login_id);
+    }
+    
+    // Set with cityConverter
+db.collection("cities").doc("LA")
+.withConverter(cityConverter)
+.set(new City("Los Angeles", "CA", "USA"));
+};
