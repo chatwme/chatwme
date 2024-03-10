8c6cc6296924c977fae2636e50a41a8f8c43e872
simbarlin <160715934+simbarlin@users.noreply.github.com>
2024-03-10 12:17:38 +0000

    unnötige gelöscht

--- chatwme/src/Login.js
+++
@@ -1,7 +0,0 @@
-
-function Login(){
-    return(
-   <a></a>// <button onClick={alert("funktioniert!")}>Default</button>
-    );
-}
-export default Login;
--- chatwme/src/Login.text.js
+++
@@ -1,6 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Login from './Login';
-
-test('renders learn react link', () => {
-  render(<Login />);
-});
